431b80efbbbdd315799db7e4d1cb3255dd36118f
Ahmad Arsyel <arsyel@agate.id>
2020-10-10 18:50:30 +0700

    implement some classes controller

--- src/ts/scenes/gameplay/GameplaySceneController.ts
+++ src/ts/scenes/gameplay/GameplaySceneController.ts
@@ -3,11 +3,21 @@ import { SceneListKey } from "../../info/GameInfo";
 import { Assets as Audio } from "../../library/AssetGameplay";
 import { BaseSceneController } from "../../modules/core/BaseSceneController";
 import { GameplaySceneView } from "./GameplaySceneView";
+import { SlimeController } from "./slime/SlimeController";
+import { BackgroundController } from "./background/BackgroundController";
+import { CheckPointController } from "./checkpoint/CheckPointController";
+import { GameController } from "./game/GameController";
+import { ObstacleController } from "./obstacle/ObstacleController";
 
 export class GameplaySceneController extends BaseSceneController {
 
 	view: GameplaySceneView;
 	audioController: AudioController;
+	gameController: GameController;
+	backgroundController: BackgroundController;
+	obstacleController: ObstacleController;
+	checkPointController: CheckPointController;
+	slimeController: SlimeController;
 
 	constructor () {
 		super(SceneListKey.GAMEPLAY);
@@ -16,6 +26,27 @@ export class GameplaySceneController extends BaseSceneController {
 	init (): void {
 		this.view = new GameplaySceneView(this);
 		this.audioController = AudioController.getInstance();
+		this.gameController = new GameController(this);
+		this.backgroundController = new BackgroundController(this);
+		this.obstacleController = new ObstacleController(this);
+		this.checkPointController = new CheckPointController(this);
+		this.slimeController = new SlimeController(this);
+
+		this.checkPointController.onClick((index, id) => {
+			console.log("Index::", index, id);
+		});
+
+		this.gameController.init();
+		this.backgroundController.init();
+		this.obstacleController.init(
+			this.backgroundController.getDisplayPercentage(),
+			this.gameController.obstacles
+		);
+		this.checkPointController.init(
+			this.backgroundController.getDisplayPercentage(),
+			this.gameController.checkpoints
+		);
+		this.slimeController.init(this.backgroundController.getDisplayPercentage());
 
 		this.onClickSFX(() => this.audioController.playSFX(Audio.sfx_click.key));
 		this.onCreateFinish(() => {});
