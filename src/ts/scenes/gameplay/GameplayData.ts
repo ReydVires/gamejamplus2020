ac78320142e76aa83de1960e8e608194007f2a94
Ahmad Arsyel <arsyel@agate.id>
2020-10-10 18:45:17 +0700

    game data bridge

---
+++ src/ts/scenes/gameplay/GameplayData.ts
@@ -0,0 +1,16 @@
+export const gameplayData = {
+
+	checkpoint: {
+		LEVEL_1: [
+			{ key: "FIRST", value:  new Phaser.Geom.Point(0.15, 0.5) },
+			{ key: "SECOND", value:  new Phaser.Geom.Point(0.15, 0.65) },
+			{ key: "THIRD", value:  new Phaser.Geom.Point(0.55, 0.8) },
+			{ key: "FOURTH", value:  new Phaser.Geom.Point(0.45, 1.15) },
+			{ key: "FIFTH", value:  new Phaser.Geom.Point(0.25, 1.35) },
+		]
+	},
+	obstacle: {
+		LEVEL_1: []
+	}
+
+};
---
+++ src/ts/scenes/gameplay/game/GameController.ts
@@ -0,0 +1,60 @@
+import { CustomTypes } from "../../../../types/custom";
+import { GameState } from "../../../info/GameInfo";
+import { gameplayData } from "../GameplayData";
+
+const EventNames = {
+	onGameStateChanged: "onGameStateChanged",
+};
+
+const OnGameStateChanged = (gameState: GameState) => {};
+
+export class GameController {
+
+	event: Phaser.Events.EventEmitter;
+	eventName: typeof EventNames;
+	private _gameState: GameState;
+
+	private _checkpoints: CustomTypes.Gameplay.GameData.Coordinate;
+	private _obstacles: CustomTypes.Gameplay.GameData.Coordinate;
+
+	constructor (private _scene: Phaser.Scene) {
+		this.event = new Phaser.Events.EventEmitter();
+		this.eventName = EventNames;
+
+		this._gameState = GameState.PREPARING;
+	}
+
+	init (): void {
+		this._checkpoints = gameplayData.checkpoint;
+		this._obstacles = gameplayData.obstacle;
+	}
+
+	get gameState (): GameState {
+		return this._gameState;
+	}
+
+	get checkpoints (): CustomTypes.Gameplay.GameData.Coordinate {
+		return this._checkpoints;
+	}
+
+	get obstacles (): CustomTypes.Gameplay.GameData.Coordinate {
+		return this._obstacles;
+	}
+
+	gameStatePlaying (): void {
+		this._gameState = GameState.PLAYING;
+	}
+
+	gameStateGameover (): void {
+		this._gameState = GameState.GAMEOVER;
+	}
+
+	gameStatePause (): void {
+		this._gameState = GameState.PAUSE;
+	}
+
+	onGameStateChanged (events: typeof OnGameStateChanged): void {
+		this.event.on(this.eventName.onGameStateChanged, events);
+	}
+
+}
