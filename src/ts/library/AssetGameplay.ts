04c6d1ae19287f7a1a38cdb84018b25f949946a4
Ahmad Arsyel <arsyel@agate.id>
2020-10-11 06:39:22 +0700

    register assets

--- src/ts/library/AssetGameplay.ts
+++ src/ts/library/AssetGameplay.ts
@@ -25,17 +25,42 @@ export const Assets: CustomTypes.Asset.ObjectAsset = {
 	baloon_decline: {
 		key: "baloon_decline",
 		url: "assets/img/slime/img_baloon_decline.png",
-		type: "STATIC" // SPRITESHEET?
+		type: "STATIC"
 	},
 	slime_char: {
 		key: "slime_char",
 		url: "assets/img/slime/spr_slime_char.png",
-		type: "STATIC" // SPRITESHEET?
+		type: "STATIC"
 	},
 	checkpoint: {
 		key: "checkpoint",
-		url: "assets/img/gui/spr_checkpoint.png",
-		type: "STATIC" // SPRITESHEET?
+		url: "assets/img/checkpoint/spr_checkpoint.png",
+		type: "STATIC"
+	},
+	exit_btn: {
+		key: "exit_btn",
+		url: "assets/img/gui/img_exit_btn.png",
+		type: "STATIC"
+	},
+	panel_buy_tool: {
+		key: "panel_buy_tool",
+		url: "assets/img/gui/img_panel_buy_tool.png",
+		type: "STATIC"
+	},
+	tools_hud: {
+		key: "tools_hud",
+		url: "assets/img/gui/img_tools_hud.png",
+		type: "STATIC"
+	},
+	tools_a: {
+		key: "tools_a",
+		url: "assets/img/tools/img_tools_a.png",
+		type: "STATIC"
+	},
+	tools_b: {
+		key: "tools_b",
+		url: "assets/img/tools/img_tools_b.png",
+		type: "STATIC"
 	},
 	sfx_click: {
 		key: "sfx_click",
