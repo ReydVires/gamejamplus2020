87a8734bb9789981a1ce7c20ea0999fd9b4ff4a3
Ahmad Arsyel <arsyel@agate.id>
2020-10-11 15:37:51 +0700

    add equip

--- src/ts/scenes/gameplay/GameplayData.ts
+++ src/ts/scenes/gameplay/GameplayData.ts
@@ -4,78 +4,87 @@ import { Assets } from "../../library/AssetGameplay";
 
 export const gameplayData = {
 
+	slimePoint: 500,
 	checkpoint: {
 		LEVEL_1: [
 			<CustomTypes.Gameplay.GameData.CheckpointInfo> {
 				key: "FIRST",
-				value: new Phaser.Geom.Point(0.175, 0.625)
+				value: new Phaser.Geom.Point(0.525, 0.38)
 			},
 			<CustomTypes.Gameplay.GameData.CheckpointInfo> {
 				key: "SECOND",
-				value: new Phaser.Geom.Point(0.2, 0.45)
+				value: new Phaser.Geom.Point(0.215, 0.175)
 			},
 			<CustomTypes.Gameplay.GameData.CheckpointInfo> {
 				key: "THIRD",
-				value: new Phaser.Geom.Point(0.65, 0.23)
-			},
-			<CustomTypes.Gameplay.GameData.CheckpointInfo> {
-				key: "FOURTH",
-				value: new Phaser.Geom.Point(0.225, 0.1)
+				value: new Phaser.Geom.Point(0.825, 0.155)
 			},
 		]
 	},
 	obstacle: {
 		LEVEL_1: [
 			<CustomTypes.Gameplay.GameData.ObstacleInfo> {
-				id: ObstaclesListKey.BOOK_A,
-				texture: Assets.obstacle_a.key,
-				position: new Phaser.Geom.Point(0.35, 0.65),
-				tools: [
-					{
-						name: ToolsListKey.LADDER,
-						cost: 90,
-						texture: Assets.tools_b.key,
-					},
-					{
-						cost: 50,
-						name: ToolsListKey.PLANK,
-						texture: Assets.tools_b.key,
-					}
-				]
-			},
-			<CustomTypes.Gameplay.GameData.ObstacleInfo> {
-				id: ObstaclesListKey.WATCH,
-				texture: Assets.obstacle_b.key,
-				position: new Phaser.Geom.Point(0.65, 0.35),
+				id: ObstaclesListKey.BOOK_GREY,
+				texture: Assets.obstacle_book_grey.key,
+				position: new Phaser.Geom.Point(0.215, 0.215),
 				tools: [
 					{
 						name: ToolsListKey.LADDER,
 						cost: 100,
-						texture: Assets.tools_a.key,
+						texture: Assets.tools_stair.key,
 					},
 					{
 						cost: 70,
 						name: ToolsListKey.PLANK,
-						texture: Assets.tools_b.key,
+						texture: Assets.tools_plane.key,
 					}
-				]
+				],
+				equip: {
+					key: Assets.equip_stair.key,
+					value: new Phaser.Geom.Point(0.215, 0.21)
+				}
 			},
 			<CustomTypes.Gameplay.GameData.ObstacleInfo> {
-				id: ObstaclesListKey.WALL,
-				texture: Assets.obstacle_c.key,
-				position: new Phaser.Geom.Point(0.5, 0.2),
+				id: ObstaclesListKey.BOOK_BROWN,
+				texture: Assets.obstacle_book_brown.key,
+				position: new Phaser.Geom.Point(0.315, 0.35),
+				tools: [
+					{
+						name: ToolsListKey.LADDER,
+						cost: 90,
+						texture: Assets.tools_stair.key,
+					},
+					{
+						cost: 50,
+						name: ToolsListKey.PLANK,
+						texture: Assets.tools_plane.key,
+					}
+				],
+				equip: {
+					key: Assets.equip_stair.key,
+					value: new Phaser.Geom.Point(0.525, 0.425)
+				}
+			},
+			<CustomTypes.Gameplay.GameData.ObstacleInfo> {
+				id: ObstaclesListKey.JUICE_CUP,
+				texture: Assets.obstacle_cup.key,
+				position: new Phaser.Geom.Point(0.875, 0.275),
 				tools: [
 					{
 						name: ToolsListKey.LADDER,
 						cost: 95,
-						texture: Assets.tools_b.key,
+						texture: Assets.tools_stair.key,
 					},
 					{
 						cost: 80,
 						name: ToolsListKey.PLANK,
-						texture: Assets.tools_a.key,
+						texture: Assets.tools_plane.key,
 					}
-				]
+				],
+				equip: {
+					key: Assets.equip_stair.key,
+					value: new Phaser.Geom.Point(0.91, 0.275)
+				}
 			},
 		]
 	},
