5381addda26092631545d2f8639238ed320a3046
Ahmad Arsyel <arsyel@agate.id>
2020-10-11 23:31:36 +0700

    update loader method

--- src/ts/scenes/loading/loader/GameplayLoaderController.ts
+++ src/ts/scenes/loading/loader/GameplayLoaderController.ts
@@ -1,13 +1,14 @@
 import { Assets } from "../../../library/AssetGameplay";
 import { LoaderHelper } from "../../../helper/LoaderHelper";
 import { BaseLoaderController } from "../../../modules/core/BaseLoaderController";
+import { CustomTypes } from "../../../../types/custom";
 
 export class GameplayLoaderController implements BaseLoaderController {
 
 	constructor (private _scene: Phaser.Scene) {}
 
 	loadResources (): void {
-		LoaderHelper.loadAssets(this._scene, Assets);
+		LoaderHelper.loadAssets(this._scene, Assets as CustomTypes.Asset.ObjectAsset);
 	}
 
 }
--- src/ts/scenes/loading/loader/TitleLoaderController.ts
+++ src/ts/scenes/loading/loader/TitleLoaderController.ts
@@ -1,13 +1,14 @@
 import { Assets } from "../../../library/AssetTitle";
 import { LoaderHelper } from "../../../helper/LoaderHelper";
 import { BaseLoaderController } from "../../../modules/core/BaseLoaderController";
+import { CustomTypes } from "../../../../types/custom";
 
 export class TitleLoaderController implements BaseLoaderController {
 
 	constructor (private _scene: Phaser.Scene) {}
 
 	loadResources (): void {
-		LoaderHelper.loadAssets(this._scene, Assets);
+		LoaderHelper.loadAssets(this._scene, Assets as CustomTypes.Asset.ObjectAsset);
 	}
 
 }
